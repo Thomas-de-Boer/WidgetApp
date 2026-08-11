324e7eb310f0dfc08fbb0c1d663caa6913fd18bb
Thomas <203590@novacollege.nl>
2026-08-11 21:09:10 +0200

    Adding functionality to navbar

--- app/src/main/java/com/example/widgetapp/MainActivity.java
+++ app/src/main/java/com/example/widgetapp/MainActivity.java
@@ -7,22 +7,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        Fragment mainFragment = new MainFragment();
+        Fragment settingsFragment = new SettingsFragment();
+        Fragment infoFragment = new InfoFragment();
+
+        setCurrentFragment(mainFragment);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.main) {
+                setCurrentFragment(mainFragment);
+            } else if (itemId == R.id.settings) {
+                setCurrentFragment(settingsFragment);
+            } else if (itemId == R.id.info) {
+                setCurrentFragment(infoFragment);
+            }
+            return true;
         });
     }
 
-    void Click() {
-        int number = 1 + 1;
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flFragment, fragment)
+                .commit();
     }
 }
