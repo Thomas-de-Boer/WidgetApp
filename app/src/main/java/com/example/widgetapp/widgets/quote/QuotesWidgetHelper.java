04d1c2b4d634b82a961d8f9be93e80971e9e3b9b
Thomas <203590@novacollege.nl>
2026-08-18 16:31:06 +0200

    Add random backgrounds

--- app/src/main/java/com/example/widgetapp/widgets/quote/QuotesWidgetHelper.java
+++ app/src/main/java/com/example/widgetapp/widgets/quote/QuotesWidgetHelper.java
@@ -16,14 +16,20 @@ public class QuotesWidgetHelper {
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_MUTABLE);
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_quotes);
-        views.setOnClickPendingIntent(R.id.widget_text, pendingIntent);
+        views.setOnClickPendingIntent(R.id.quote, pendingIntent);
 
         String[] quotes = context.getResources().getStringArray(R.array.quotes);
 
         String randomQuote = quotes[new Random().nextInt(quotes.length)];
 
+        int[] images = {R.drawable.quote_background1, R.drawable.quote_background2, R.drawable.quote_background3, R.drawable.quote_background4, R.drawable.quote_background5, R.drawable.quote_background6};
 
-        views.setTextViewText(R.id.widget_text, randomQuote);
+        int randomImage = images[new Random().nextInt(images.length)];
+
+
+        views.setTextViewText(R.id.quote, randomQuote);
+
+        views.setInt(R.id.quote_widget, "setBackgroundResource", randomImage);
 
         return views;
     }
